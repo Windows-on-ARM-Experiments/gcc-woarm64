62f01e7dc7b57a8a95a4978197e033a17db5d188
Evgeny Karpov <eukarpov@gmail.com>
2024-01-29 16:43:12 +0100

    [PATCH 13/16] Add named section and funtion type for Cygwin Aarch64
    Support for function type declaration and named sections.

--- gcc/config/aarch64/cygming.h
+++ gcc/config/aarch64/cygming.h
@@ -35,11 +35,20 @@ along with GCC; see the file COPYING3.  If not see
 #include <stdio.h>
 #endif
 
+extern void aarch64_pe_asm_named_section (const char *, unsigned int, tree);
+extern void aarch64_pe_declare_function_type (FILE *file, const char *name, int pub);
+
+#define TARGET_ASM_NAMED_SECTION  aarch64_pe_asm_named_section
+
 /* Select attributes for named sections.  */
 #define TARGET_SECTION_TYPE_FLAGS  aarch64_pe_section_type_flags
 
 #define TARGET_ASM_UNIQUE_SECTION aarch64_pe_unique_section
 #define TARGET_ENCODE_SECTION_INFO  aarch64_pe_encode_section_info
+
+/* Declare the type properly for any external libcall.  */
+#define ASM_OUTPUT_EXTERNAL_LIBCALL(FILE, FUN) \
+  aarch64_pe_declare_function_type (FILE, XSTR (FUN, 0), 1)
 
 #define TARGET_OS_CPP_BUILTINS()                                 \
   do                                                            \
