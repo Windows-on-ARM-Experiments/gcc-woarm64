3e54c35887eabe9c5a0a6bda479c4e3cfb4c158c
Evgeny Karpov <eukarpov@gmail.com>
2024-04-08 18:31:23 +0200

    Replace the original ix86 implementation for DLL import/export with MinGW.
    This patch replaces the original DLL import/export implementation in
    ix86 with mingw. The mingw implementation was introduced based on
    the ix86 implementation and aims to be reused in multiple targets.
    It has minor changes compared to the original one.
    
    gcc/ChangeLog:
    
    	* config.gcc: Add winnt-dll.o, which contains the DLL
    	import/export implementation.
    	* config/i386/i386-expand.cc (ix86_expand_move): Remove the
    	old implementation. Rename the required function to MinGW.
    	Rename it to a conditional function that will reuse the
    	MinGW implementation for COFF and nothing otherwise.
    	* config/i386/i386-expand.h (is_imported_p): Likewise.
    	(mingw_GOT_alias_set): Likewise.
    	(ix86_legitimize_pe_coff_symbol): Likewise.
    	* config/i386/i386.cc (is_imported_p): Likewise.
    	(ix86_legitimize_pe_coff_symbol): Likewise.
    	(ix86_GOT_alias_set): Likewise.
    	(legitimize_pic_address): Likewise.
    	(struct dllimport_hasher):
    	(GTY): Likewise.
    	(get_dllimport_decl): Likewise.
    	(legitimize_pe_coff_extern_decl): Likewise.
    	(legitimize_dllimport_symbol): Likewise.
    	(legitimize_pe_coff_symbol): Likewise.
    	(ix86_legitimize_address): Likewise.
    	* config/mingw/t-cygming: Add the winnt-dll.o compilation.

--- gcc/config/i386/i386-protos.h
+++ gcc/config/i386/i386-protos.h
@@ -18,6 +18,7 @@ along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
 #include "config/mingw/winnt.h"
+#include "config/mingw/winnt-dll.h"
 
 /* In i386-common.cc.  */
 extern bool ix86_handle_option (struct gcc_options *opts,
